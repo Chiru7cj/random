21b0055ffdc3a6461a546921aab2d147af1af234
knoldus <chiranjeev.kumar@knoldus.com>
2021-10-26 17:54:12 +0530

    sec

---
+++ Question15.java
@@ -0,0 +1,30 @@
+package core;
+
+public class Question15 {
+    public static void main(String[] args) {
+
+        try{
+            int a[]=new int[5];
+            a[5]=30/0;
+            System.out.println(a);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Arithmetic Exception occurs");
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("ArrayIndexOutOfBounds Exception occurs");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Parent Exception occurs");
+        }
+        finally{
+            int a=5;   //finally block executed
+            int b=6;
+            int c=a+b;
+            System.out.println(c);
+        }
+    }
+}
